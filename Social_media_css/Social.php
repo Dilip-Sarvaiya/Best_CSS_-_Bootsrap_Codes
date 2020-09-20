6936288663b4cc13011db16685d0ff4a750199ab
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-09-20 11:47:26 +0530

    Hover effect for Social Media Icons such as Facebook,Instagram,Whatsapp,Twitter,You tube

---
+++ Social_media_css/Social.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1.0">
+		<title>Dilip CSS</title>
+		<link rel="stylesheet" type="text/css" href="social.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+	</head>
+	<body>
+		<h1>Dilip Sarvaiya</h1>
+		<div class="social-media">
+			<a href="#"><i class="fab fa-facebook"></i></a>
+			<a href="#"><i class="fab fa-twitter"></i></a>
+			<a href="#"><i class="fab fa-whatsapp"></i></a>
+			<a href="#"><i class="fab fa-instagram"></i></a>
+			<a href="#"><i class="fab fa-youtube"></i></a>
+
+			
+		</div>
+	</body>
+</html>	
